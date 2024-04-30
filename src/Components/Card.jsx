79fd79e25eab69b0bff6fae0dfbbf3eabbd848ca
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-04-30 13:04:43 +0530

    Commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { IoIosDocument } from "react-icons/io";
 import { IoClose } from "react-icons/io5";
+import { FaAngleDown } from "react-icons/fa6";
 
 
 const Card = () => {
@@ -9,7 +10,22 @@ const Card = () => {
         <div className="top">
            <IoIosDocument className='doc'/>
            <IoClose className='close'/> 
-        </div>        
+        </div>   
+        <div className="middle">
+        
+        </div>
+        <div className="bot"> 
+         <div className="lb">
+            <div className="dot"></div>
+            <h3>Pending</h3>
+         </div>
+         <div className="rb">
+              <h4>Select Status</h4> 
+              <FaAngleDown className='down'/>
+         </div>
+           
+            
+        </div>     
     </div>
   )
 }
