0c2f9a4c3b364bc29eb55699bfbb5799750b762d
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-04-30 11:48:12 +0530

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,19 @@
 import React,{useState} from 'react'
 import "../style.scss"
+import { IoMdAdd } from "react-icons/io";
+
 
 
 const Navbar = () => {
     //set Form
     const[cf,setCf] = useState(true);
+    //active 
+    const[active,setActive] = useState(false);
   return (
     <div className="navbar">
          <h3>Document</h3>
          <div className="add">
-             <button onClick={() => setCf(!cf)}>Add</button>
+             <button onClick={() => setCf(!cf)}><IoMdAdd className={cf ? 'addbtn activa' : 'addbtn'}/></button>
             {cf && (<div className="cardForm">
                   <input type="text" name='note' placeholder='Write your short note...📝' />
                   <button>Add Note</button>
